bfb3716b5d74f9089915d960a05d270b9cc4f137
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-05-22 23:49:30 -0700

    fetch data

--- src/datatablesource.js
+++ src/datatablesource.js
@@ -20,8 +20,8 @@ export const userColumns = [
   },
 
   {
-    field: "age",
-    headerName: "Age",
+    field: "address",
+    headerName: "Address",
     width: 100,
   },
   {
@@ -35,89 +35,5 @@ export const userColumns = [
         </div>
       );
     },
-  },
-];
-
-//temporary data
-export const userRows = [
-  {
-    id: 1,
-    username: "Snow",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    status: "active",
-    email: "1snow@gmail.com",
-    age: 35,
-  },
-  {
-    id: 2,
-    username: "Jamie Lannister",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    email: "2snow@gmail.com",
-    status: "passive",
-    age: 42,
-  },
-  {
-    id: 3,
-    username: "Lannister",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    email: "3snow@gmail.com",
-    status: "pending",
-    age: 45,
-  },
-  {
-    id: 4,
-    username: "Stark",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    email: "4snow@gmail.com",
-    status: "active",
-    age: 16,
-  },
-  {
-    id: 5,
-    username: "Targaryen",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    email: "5snow@gmail.com",
-    status: "passive",
-    age: 22,
-  },
-  {
-    id: 6,
-    username: "Melisandre",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    email: "6snow@gmail.com",
-    status: "active",
-    age: 15,
-  },
-  {
-    id: 7,
-    username: "Clifford",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    email: "7snow@gmail.com",
-    status: "passive",
-    age: 44,
-  },
-  {
-    id: 8,
-    username: "Frances",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    email: "8snow@gmail.com",
-    status: "active",
-    age: 36,
-  },
-  {
-    id: 9,
-    username: "Roxie",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    email: "snow@gmail.com",
-    status: "pending",
-    age: 65,
-  },
-  {
-    id: 10,
-    username: "Roxie",
-    img: "https://live.staticflickr.com/65535/52001067671_70c3cd3098_z.jpg",
-    email: "snow@gmail.com",
-    status: "active",
-    age: 65,
   },
 ];
